889e58791ac0fad682179ea1e59987829a1dd6e3
Saadat Khan <saadatkhan6263@example.com>
2024-09-02 03:10:06 -0700

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./index.html'];
+export const theme = {
+  extend: {
+    colors: {
+      'primary': '#3238f2',
+      'saadat': '#E4003A',
+    },
+    fontFamily: {
+      'display': [' Poppins ', 'sans-serif'],
+      'body': [' Inter ', 'sans-serif']
+    }
+  },
+};
+export const plugins = [];
+
